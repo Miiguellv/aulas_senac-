865963dc101bebc854d700c52c7c68ef7352327c
Talentos 08103 <UG_TALENTOS_E08103@asav.brm>
2024-04-16 11:51:15 -0300

    teste 6

---
+++ template_javascript/index.js
@@ -0,0 +1,46 @@
+/*Let primeiroNumero
+let segundoNumero
+let resultado
+
+primeiroNumero = prompt("Digite o primeiro numero")
+segundoNumero = prompt ("Digite o segundo numero")
+
+resultado = primeiroNumero === segundoNumero
+alert(resultado)*/
+
+/*let a 
+let b 
+let c 
+let resultado
+
+a = true 
+b = false 
+c = true 
+
+resultado = a && b 
+alert(resultado)
+
+b && c 
+a && c 
+a && b && c */
+
+let nome 
+let anoNacimento
+let anoAtual
+let idade
+let maiorIdade
+let idade2050
+
+
+nome = prompt("Digite seu nome")
+anoNacimento = prompt("Digite sua data de nascimento")
+anoAtual = prompt("Digite o ano atual")
+idade = anoAtual - anoNacimento
+maiorIdade = idade >= 18
+idade2050 = 2050 - anoNacimento
+
+alert(nome)
+alert(idade)
+alert(maiorIdade)
+alert(idade2050)
+
